ce23e983562e57f09380eb0c26e5cd776f84db70
Copper Phosphate <cu3po42@gmail.com>
2014-10-31 12:52:03 +0100

    Extend Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,13 +32,40 @@ module.exports = function(grunt) {
                 src: "src/index.html",
                 dest: "dist/prod/"
             }
+        },
+        htmlmin: {
+            prod: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    "dist/prod/index.html": "src/index.html"
+                }
+            }
+        },
+        uglify: {
+            prod: {
+                options: {
+                    mangle: false
+                },
+                files: {
+                    expand: true,
+                    cwd: "src/scripts",
+                    src: "**/*.js",
+                    dest: "dist/prod/scripts"
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-htmlmin");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
 
     grunt.registerTask("default", ["clean:debug", "less:debug", "copy:debug"]);
+    grunt.registerTask("prod", ["clean:prod", "less:prod", "htmlmin:prod", "uglify:prod"]);
 }
 
